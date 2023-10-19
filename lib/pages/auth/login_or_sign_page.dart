a6b4451304be04db8fbcc5f5328284a383f10e18
apsarabiswokarma <apsarabk94@gmail.com>
2023-10-19 20:20:49 +0545

    login or signup

--- lib/pages/auth/login_or_sign_page.dart
+++ lib/pages/auth/login_or_sign_page.dart
@@ -8,7 +8,7 @@ class LoginSignUpPage extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/LoginSignupScreen.png'),
+          image: AssetImage('assets/images/LoginSignupScreenbgi.png'),
         ),
       ),
       child: Column(
@@ -18,6 +18,9 @@ class LoginSignUpPage extends StatelessWidget {
             "Let's Start",
             style: TextStyle(
               fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              decoration: TextDecoration.none,
             ),
           ),
           Image.asset(
@@ -26,6 +29,31 @@ class LoginSignUpPage extends StatelessWidget {
             width: 250,
           ),
           const Spacer(),
+          ElevatedButton(
+            onPressed: () {
+              // Add login logic here.
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple,
+            ),
+            child: const Text("Login"),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            //width: double.maxFinite,
+            child: ElevatedButton(
+              onPressed: () {
+                // Add your signup logic here.
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+              ),
+              child: const Text(
+                "Signup",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/pages/splash_screen/splash_page_2.dart
+++ lib/pages/splash_screen/splash_page_2.dart
@@ -1,48 +1,67 @@
 import 'package:flutter/material.dart';
 
 class SplashPage2 extends StatelessWidget {
-  const SplashPage2({super.key});
+  const SplashPage2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/SplashScreen.png'),
-          fit: BoxFit.cover,
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/SplashScreen.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
-      child: SafeArea(
-        child: Column(
-          children: [
-            const Spacer(),
-            const Text(
-              "Skip",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
+        SafeArea(
+          child: Column(
+            children: [
+              const Spacer(),
+              const Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "Skip",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Image.asset(
+                'assets/images/splashimage2.png',
+                height: 250,
+              ),
+              const Spacer(),
+              const Text(
+                "Empowering Your SAT Success",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+        const Positioned(
+          bottom: 30,
+          right: 16,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                Icons.arrow_forward,
                 color: Colors.white,
               ),
-              textAlign: TextAlign.right,
-            ),
-            Image.asset(
-              'assets/images/splashimage2.png',
-              height: 250,
-            ),
-            const Spacer(),
-            const Text(
-              "Empowering Your SAT Success",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Colors.white,
-              ),
-            ),
-            const Spacer(),
-          ],
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
