f14bd2fd420bb2b3029db34884f35294120691b9
apsarabiswokarma <apsarabk94@gmail.com>
2023-10-20 19:28:48 +0545

    login generic design

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -5,32 +5,80 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/AuthScreen.png'),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/AuthScreen.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: const Column(
-        children: [
-          Text(
-            "Login",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black, decoration: TextDecoration.none),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Text(
-            "Welcome back! Access your SAT preparation journey ",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
-              decoration: TextDecoration.none,
+        child: Column(
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black, decoration: TextDecoration.none),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              "Welcome back! Access your SAT preparation journey ",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+                decoration: TextDecoration.none,
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true, // to hide the password input
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Forgot Password?'),
+            ),
+            const SizedBox(height: 16),
+            const Text("or"),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+              ),
+              child: const Text('Continue with Google'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+              ),
+              child: const Text('Continue with Apple ID'),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Donot have an account? Sign Up instead'),
+            ),
+          ],
+        ),
       ),
     );
   }
