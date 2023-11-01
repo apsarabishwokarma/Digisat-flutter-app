3c48262474cd0b97923a543523000869998260ed
apsarabiswokarma <apsarabk94@gmail.com>
2023-11-01 19:13:39 +0545

    formpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:digisat_app/pages/auth/forget_password_screen.dart';
+import 'package:digisat_app/pages/auth/signup_form_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +7,12 @@ void main() {
 
 class DigiSat extends StatelessWidget {
   const DigiSat({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'DigiSat',
       debugShowCheckedModeBanner: false,
-      home: ForgetPassword(),
+      home: FormPage(),
     );
   }
 }
--- lib/pages/auth/signup_form_screen.dart
+++ lib/pages/auth/signup_form_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
 
-class FormPage extends StatelessWidget {
-  const FormPage({super.key});
+class FormPage extends StatefulWidget {
+  const FormPage({Key? key}) : super(key: key);
+
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  String? _selectedCountry;
+  String? _selectedGender;
+
+  final List<String> countries = ['Nepal', 'USA', 'UK'];
+  final List<String> genders = ['Male', 'Female', 'Other'];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Details Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Full Name'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Phone Number'),
+            ),
+            DropdownButtonFormField<String>(
+              value: _selectedCountry,
+              items: countries.map((country) {
+                return DropdownMenuItem<String>(
+                  value: country,
+                  child: Text(country),
+                );
+              }).toList(),
+              hint: const Text('Country'),
+              onChanged: (value) {
+                setState(() {
+                  _selectedCountry = value;
+                });
+              },
+            ),
+            DropdownButtonFormField<String>(
+              value: _selectedGender,
+              items: genders.map((gender) {
+                return DropdownMenuItem<String>(
+                  value: gender,
+                  child: Text(gender),
+                );
+              }).toList(),
+              hint: const Text('Gender'),
+              onChanged: (value) {
+                setState(() {
+                  _selectedGender = value;
+                });
+              },
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
