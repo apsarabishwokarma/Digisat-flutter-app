ddd1a273870d3fd31087a166dcc19b8372ed4300
apsarabiswokarma <apsarabk94@gmail.com>
2023-11-02 20:54:29 +0545

    colors shades

--- lib/auth/signup_form_screen.dart
+++ lib/auth/signup_form_screen.dart
@@ -8,6 +8,7 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String? _selectedCountry;
   String? _selectedGender;
 
@@ -22,54 +23,82 @@ class _FormPageState extends State<FormPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Full Name'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Phone Number'),
-            ),
-            DropdownButtonFormField<String>(
-              value: _selectedCountry,
-              items: countries.map((country) {
-                return DropdownMenuItem<String>(
-                  value: country,
-                  child: Text(country),
-                );
-              }).toList(),
-              hint: const Text('Country'),
-              onChanged: (value) {
-                setState(() {
-                  _selectedCountry = value;
-                });
-              },
-            ),
-            DropdownButtonFormField<String>(
-              value: _selectedGender,
-              items: genders.map((gender) {
-                return DropdownMenuItem<String>(
-                  value: gender,
-                  child: Text(gender),
-                );
-              }).toList(),
-              hint: const Text('Gender'),
-              onChanged: (value) {
-                setState(() {
-                  _selectedGender = value;
-                });
-              },
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Submit'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Full Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your full name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email address';
+                  }
+
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Phone Number'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+
+                  return null;
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _selectedCountry,
+                items: countries.map((country) {
+                  return DropdownMenuItem<String>(
+                    value: country,
+                    child: Text(country),
+                  );
+                }).toList(),
+                hint: const Text('Country'),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCountry = value;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _selectedGender,
+                items: genders.map((gender) {
+                  return DropdownMenuItem<String>(
+                    value: gender,
+                    child: Text(gender),
+                  );
+                }).toList(),
+                hint: const Text('Gender'),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // The form is valid, and you can process the data.
+                    // Typically, you'd submit the data to a server or perform other actions here.
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/material.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor violet = MaterialColor(
+  0xFF8A2BE2, // Primary color value
+  <int, Color>{
+    50: Color(0xFFE6D3FF), // Shade 50
+    100: Color(0xFFC8A6FF), // Shade 100
+    200: Color(0xFFA97AFF), // Shade 200
+    300: Color(0xFF8A4EFF), // Shade 300
+    400: Color(0xFF6C21FF), // Shade 400
+    500: Color(0xFF8A2BE2), // Primary shade 500
+    600: Color(0xFF5D00E6), // Shade 600
+    700: Color(0xFF4A00B4), // Shade 700
+    800: Color(0xFF350081), // Shade 800
+    900: Color(0xFF23004F), // Shade 900
+  },
+);
